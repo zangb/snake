ef327dcc27516a3bc4c749f8a8ef2d3e5eda56e6
Jürgen Markl <juergen.markl@st.oth-regensburg.de>
2019-07-14 19:28:43 +0200

    minor changes in exception, mbox still doesn't work

--- Exception.h
+++ Exception.h
@@ -3,13 +3,14 @@
 #include <iostream>
 #include "Textbox.h"
 
+using namespace std;
 
 class CollisionExcept : std::exception{
 private:
     int error;
     Textbox* mbox;
 public:
-    CollisionExcept(int Error, Textbox* messagebox);
+    CollisionExcept(int Error, Textbox*& messagebox);
     void what();
     int GetError();
 };
@@ -18,13 +19,14 @@ int CollisionExcept::GetError(){
     return error;
 }
 
-CollisionExcept::CollisionExcept(int Error, Textbox* messagebox){
+CollisionExcept::CollisionExcept(int Error, Textbox*& messagebox){
     error = Error;
     mbox = messagebox;
 }
 void CollisionExcept::what(){
     switch(GetError()){
         case 1: 
+        cout << "Collision with your body!" << endl;
         mbox = new Textbox("You collided with your own body",Punkt(250,250), Punkt(30,30));
         
         break;
